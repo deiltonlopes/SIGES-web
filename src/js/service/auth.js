1d71f9de77591a51498a844eebfd0d5a90c74c74
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-05-26 10:41:46 -0300

    auth done

--- src/js/service/auth.js
+++ src/js/service/auth.js
@@ -1,11 +1,62 @@
 
-function createUser(){
-    let email = document.getElementById("emailInput").value;
-    let password = document.getElementById("passwordInput").value;
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(user=>{
-        console.log(user);
-    }).catch(err=>{
-        console.log(err);
-    });
+function createUser(email, password){
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
 };
+
+
+function login(email, password){
+    return firebase.auth().loginWithEmailAndPassword(email, password);
+};
+
+function facebookLogin(){
+    const provider = new firebase.auth.FacebookAuthProvider();
+    return firebase.auth().signInWithPopup(provider);
+};
+
+function gmailLogin(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth().signInWithPopup(provider);
+};
+
+function logout(){
+    return firebase.auth().signOut();
+};
+
+function currentUser(){
+    firebase.auth().languageCode = 'pt';
+    return firebase.auth().currentUser;
+};
+
+function updateUser(field, newInfo){
+    let user = currentUser();
+    if(user){
+        let functions = {
+            'profile': user.updateProfile,
+            'email': user.updateEmail,
+            'password': user.updatePassword,
+            'phone': user.updatePhoneNumber
+        };
+        functions[field](newInfo);
+    }
+};
+
+function deleteUser(){
+    let user = currentUser();
+    if(user){
+        return user.delete();
+    }
+};
+
+function verifyUser(){
+    let user = currentUser();
+    if(!user.emailVerified){
+        return user.sendEmailVerification();
+    }
+};
+
+function recoverPassword(){
+    let user = currentUser();
+    if(user){
+        return firebase.auth().sendPasswordResetEmail(user.email);
+    }
+}
